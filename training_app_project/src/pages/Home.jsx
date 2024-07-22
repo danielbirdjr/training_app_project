633f27b0d9ed5869886c27a9aed72a6bed705d02
Daniel Bird <danielbirdpersonal@gmail.com>
2024-07-22 13:14:20 -0400

    Got all of the links and images to load. Still need to figure out why the font weight is different on the React Version than the HTML/CSS version

--- training_app_project/src/pages/Home.jsx
+++ training_app_project/src/pages/Home.jsx
@@ -4,20 +4,23 @@ import React from "react";
 
 const Home = () => {
     return (
-        <section className="first_section home" >
-            <div className="home_text">
-                <div className="home_text_heading">
-                    <h1>The most intelligent program for you</h1>
+        <>
+            <div className="first_section"></div>
+            <section className="first_section home" >
+                <div className="home_text">
+                    <div className="home_text_heading">
+                        <h1>The most intelligent program for you</h1>
+                    </div>
+                    <div className="home_text_and_button">
+                        <p>ProgressAI is an innovative fitness tracking app that helps users to take their fitness goals. With its minimalist interface, ProgressAI offers customized workout plans, progress tracking, and AI driven insights to help you reach your fitness goals.</p>
+                        <button className="btn" onClick={() => window.location.href='/sign-up'}>Try For Free</button>
+                    </div>
                 </div>
-                <div className="home_text_and_button">
-                    <p>ProgressAI is an innovative fitness tracking app that helps users to take their fitness goals. With its minimalist interface, ProgressAI offers customized workout plans, progress tracking, and AI driven insights to help you reach your fitness goals.</p>
-                    <button className="btn" onclick="location.href='/sign-up.html'">Try For Free</button>
+                <div className="home_image">
+                    <img src="" alt="" />
                 </div>
-            </div>
-            <div className="home_image">
-                <img src="" alt="" />
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
